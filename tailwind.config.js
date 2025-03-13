9750702239ce899fe1d8820f1b2e669dccec901b
BAD-WOLF <matheusviaira160@gmail.com>
2025-03-13 20:25:52 +0100

    feat(tailwind-config) Add color default

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "color_gray_200":"#F0F4F8",
+        "color_gray_400":"#3D3D3D",
+        "color_gray_500":"#02060c",
+        "ciano_escuro": "#0E7490",
+        "azul_petroleo": "#274C7C"
+      }
+    },
   },
   plugins: [],
 }
