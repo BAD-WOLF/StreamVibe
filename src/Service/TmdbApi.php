7744bb2c054348461c39824d7e96616fdae37838
matheu <matheusviaira160@gmail.com>
2025-03-18 10:33:42 -0300

    perf: ⚡️ Englobalization use namespaces

--- src/Service/TmdbApi.php
+++ src/Service/TmdbApi.php
@@ -2,11 +2,13 @@
 
 namespace App\Service {
 
-    use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-    use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
-    use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-    use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-    use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+    use Symfony\Contracts\HttpClient\Exception\{
+        ClientExceptionInterface,
+        DecodingExceptionInterface,
+        RedirectionExceptionInterface,
+        ServerExceptionInterface,
+        TransportExceptionInterface,
+    };
     use Symfony\Contracts\HttpClient\HttpClientInterface;
 
     class TmdbApi
