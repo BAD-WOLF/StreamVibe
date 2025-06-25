6faae7b0a63ed3d3a5642af0ce4d20a6e7386a1e
matheu <matheusviaira160@gmail.com>
2025-06-25 14:13:34 -0300

    feat: 🎸 add JWT refresh token support
    - Added `gesdinet/jwt-refresh-token-bundle` to `composer.json` dependencies
    - Registered `GesdinetJWTRefreshTokenBundle` in `config/bundles.php`
    - Created `config/packages/gesdinet_jwt_refresh_token.yaml` with refresh token class configuration
    - Configured `refresh_jwt` under the `api` firewall in `security.yaml`
    - Added route `/api/token/refresh` in `config/routes/gesdinet_jwt_refresh_token.yaml`
    - Created Doctrine entity `App\Entity\RefreshToken` extending bundle's base class
    - Generated migration to create `refresh_tokens` table in the database

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
